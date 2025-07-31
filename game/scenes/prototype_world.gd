d9f25ca8d4be9194e4a3d240b7adaa8b6f65d363
Myr the Moth <myrthemoth@gmail.com>
2025-07-31 22:26:33 +0100

    fixes fixes and more fixes

--- game/scenes/prototype_world.gd
+++ game/scenes/prototype_world.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-@onready var camera_2d: Camera2D = $Camera2D
-@onready var color_rect: ColorRect = $ColorRect
-
 
 func _enter_tree() -> void:
 	add_to_group("world")
@@ -13,5 +10,4 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	if camera_2d.global_position != (color_rect.size / 2):
-		camera_2d.global_position = (color_rect.size / 2)
+	pass
